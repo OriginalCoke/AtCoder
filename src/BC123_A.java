53c0df9ed22c098a643456586594e0967c4887e9
OriginalCoke <whuang36@hawk.iit.edu>
2019-04-08 15:51:28 -0500

    Five Antennas

---
+++ src/BC123_A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BC123_A {
+    //Five Antennas
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] A = new int[5];
+        for (int i = 0; i < 5; i++) {
+            A[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int max = A[4] - A[0];
+        if (max <= k) System.out.println("Yay!");
+        else System.out.println(":(");
+        sc.close();
+    }
+}
