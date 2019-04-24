d805bc404dafbaa794cd1e78f8375710e5f26fd0
OriginalCoke <whuang36@hawk.iit.edu>
2019-04-23 19:59:52 -0500

    Buttons

---
+++ src/BC124_A.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BC124_A {
+    //Buttons
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(helper(a, b));
+        sc.close();
+    }
+
+    public static int helper(int a, int b) {
+        if (a < b) return helper(b, a);
+        if (a - b > 1) return 2 * a - 1;
+        else return a + b;
+    }
+}
