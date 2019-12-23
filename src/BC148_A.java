bf6628ebf18483810afa44ec6311beeb2b9ed661
OriginalCoke <whuang36@hawk.iit.edu>
2019-12-22 20:09:20 -0600

    Round One

---
+++ src/BC148_A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BC148_A {
+    //Round One
+    //我只能做这么简单的题目了吗?
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a != 1 && b != 1) System.out.println("1");
+        else if (a != 2 && b != 2) System.out.println("2");
+        else if (a != 3 && b != 3) System.out.println("3");
+        sc.close();
+    }
+}
